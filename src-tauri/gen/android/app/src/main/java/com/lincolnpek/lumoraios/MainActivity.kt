e44b19d9d807797e58c072a9fb00d67615134a19
lincon07 <73676604+lincon07@users.noreply.github.com>
2026-06-22 06:45:04 -0700

    android init

---
+++ src-tauri/gen/android/app/src/main/java/com/lincolnpek/lumoraios/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lincolnpek.lumoraios
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
